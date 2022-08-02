c9615515d0c97d50a66e6e41b7f50a9abf3be554
WebClinic <59230387+webclinic017@users.noreply.github.com>
2022-08-02 19:13:58 +0400

    Update header.sh

--- header.sh
+++ header.sh
@@ -9,7 +9,8 @@ major_version="$(echo "${KERNEL_VERSION}" | awk -vFS=. '{ print $1 }')"
 apt-get install -y build-essential bc curl flex bison libelf-dev
 
 mkdir -p /usr/src/linux
-curl -sL "https://www.kernel.org/pub/linux/kernel/v${major_version}.x/linux-$kernel_version.tar.gz"     | tar --strip-components=1 -xzf - -C /usr/src/linux
+#curl -sL "https://www.kernel.org/pub/linux/kernel/v${major_version}.x/linux-$kernel_version.tar.gz"     | tar --strip-components=1 -xzf - -C /usr/src/linux
+curl -sL "https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.19.tar.gz"     | tar --strip-components=1 -xzf - -C /usr/src/linux
 cd /usr/src/linux
 zcat /proc/config.gz > .config
 make ARCH=x86 oldconfig
