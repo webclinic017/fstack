40ab369ccd902c716fe11a21c279f40730accf8f
WebClinic <59230387+webclinic017@users.noreply.github.com>
2022-08-02 18:00:30 +0400

    Update fstack.sh

--- fstack.sh
+++ fstack.sh
@@ -13,9 +13,9 @@ sudo apt-get -q upgrade -y
 sudo apt-get -q install -y \
      make meson git gcc openssl libssl-dev \
      bc libnuma1 libnuma-dev libpcre3 libpcre3-dev zlib1g-dev \
-     net-tools
+     net-tools pkg-config \
 
-sudo apt-get -q install -y pkg-config python 
+sudo apt-get -q install -y  python
 ################################################################################
 # Download kernel headers
 ################################################################################
