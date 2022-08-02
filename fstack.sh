d24ae770fb0bb9b9fdd9e094cdb1fc4f405ca65c
WebClinic <59230387+webclinic017@users.noreply.github.com>
2022-08-02 17:58:02 +0400

    Update fstack.sh

--- fstack.sh
+++ fstack.sh
@@ -12,7 +12,7 @@ sudo apt-get -q upgrade -y
 
 sudo apt-get -q install -y \
      make meson git gcc openssl libssl-dev \
-     bc libnuma1 libnuma-dev libpcre3 libpcre3-dev zlib1g-dev python \
+     bc libnuma1 libnuma-dev libpcre3 libpcre3-dev zlib1g-dev \
      net-tools pkg-config
 
 ################################################################################
